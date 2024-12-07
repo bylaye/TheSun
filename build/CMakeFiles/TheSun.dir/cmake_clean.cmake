6286849577bcbd9d5814c5ae9ebcf8c39300a92b
bylaye <abdoulayeniang2010@gmail.com>
2024-12-07 18:07:01 +0100

    build add update

---
+++ build/CMakeFiles/TheSun.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TheSun.dir/src/Objects.cpp.o"
+  "CMakeFiles/TheSun.dir/src/Objects.cpp.o.d"
+  "CMakeFiles/TheSun.dir/src/main.cpp.o"
+  "CMakeFiles/TheSun.dir/src/main.cpp.o.d"
+  "TheSun"
+  "TheSun.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TheSun.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
