98fc2c251caced0769226040d423e3bea2a97811
bylaye <abdoulayeniang2010@gmail.com>
2024-12-10 01:22:08 +0100

    reajust color planet

--- src/Objects.cpp
+++ src/Objects.cpp
@@ -9,10 +9,10 @@ Objects::Objects(std::string name, double mass, double radius, double distanceSu
     shape.setOrigin(radius/radiusScale, radius/radiusScale);
     //shape.setPosition(positionX / , positionY);
     shape.setFillColor(shapeColor);
-    shape.setScale(1, 1.2);
+    shape.setScale(1, 1.3);
     //shape.setRadius(70);
     //shape.setOrigin(70, 100);
-    shape.setPosition(70, 400);
+    shape.setPosition(100, 400);
     revolution = 0;
     angleOrbit = 0;
 };
@@ -24,10 +24,10 @@ const std::vector<PLANET> PlanetVector {
     {"Venus",   4.8675e24, 6.0518e6, 1.082e11, sf::Color::Yellow}, // Planet 2
     {"Earth",   5.9724e24, 6.3710e6, 1.496e11, sf::Color::Blue}, // Planet 3: Our Planet
     {"Mars",    6.4171e23, 3.3895e6, 2.279e11, sf::Color::Red}, // Planet 4
-    {"Jupiter", 1.8982e27, 6.9911e7, 7.785e11, sf::Color(100, 150, 0, 90)}, // Planet 5
-    {"Saturn",  5.6834e26, 5.8232e7, 1.433e12, sf::Color(100, 150, 0, 90)}, // Planet 6
-    {"Uranus",  8.6810e25, 2.5362e7, 2.877e12, sf::Color(100, 150, 0, 90)}, // Planet 7
-    {"Neptune", 1.0241e26, 2.4622e7, 4.503e12, sf::Color(100, 150, 0, 90)}, // Planet 8
+    {"Jupiter", 1.8982e27, 6.9911e7, 7.785e11, sf::Color(100, 50, 50)}, // Planet 5
+    {"Saturn",  5.6834e26, 5.8232e7, 1.433e12, sf::Color(100, 10, 150)}, // Planet 6
+    {"Uranus",  8.6810e25, 2.5362e7, 2.877e12, sf::Color(100, 50, 200, 90)}, // Planet 7
+    {"Neptune", 1.0241e26, 2.4622e7, 4.503e12, sf::Color(0, 0, 200, 90)}, // Planet 8
 };
 
 
